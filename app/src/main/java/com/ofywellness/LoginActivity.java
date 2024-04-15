313998effc66917fc15f78ead96e3357950acdad
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-04-15 17:19:06 +0530

    New App Google sign-in and register page frontend complete

---
+++ app/src/main/java/com/ofywellness/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.ofywellness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity {
+    private GoogleSignInClient ofyGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Implementing Google Sign-In
+        // Objects required
+        SignInButton ofyGoogleSignInButton = findViewById(R.id.google_image);
+        GoogleSignInOptions ofyGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        ofyGoogleSignInClient = GoogleSignIn.getClient(this, ofyGoogleSignInOptions);
+
+        //onClick for Google Sign-In Button
+        ofyGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = ofyGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 1000);
+            }
+        });
+
+    }
+
+    // To handle Google Sign-In result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check if the requested code is same as needed
+        if (requestCode == 1000) {
+            try {
+                //Get Signed Account
+                GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult();
+                // Finish current activity
+                finish();
+                // Show a Toast message
+                Toast.makeText(getApplicationContext(), "Logged In as " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+                // Start new activity
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            } catch (Exception e) {
+                //Log Error
+                Log.e(" Error ", "Error Signing in to Google *******************************************************************");
+                // Print full Error
+                e.printStackTrace();
+                // Show a Toast message
+                Toast.makeText(getApplicationContext(), "Something Went Wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
