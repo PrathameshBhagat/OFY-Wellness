730cc038abae714cfc189cfd1f361bdf76e156e6
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-04-19 12:19:58 +0530

    Image uploading working see full commits for all details, comments required, HOMEACTIVITY()(1|2), track diet required

--- app/src/main/java/com/ofywellness/fragments/AddMealTab.java
+++ app/src/main/java/com/ofywellness/fragments/AddMealTab.java
@@ -1,15 +1,30 @@
 package com.ofywellness.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.ofywellness.R;
 import com.ofywellness.db.ofyDatabase;
 import com.ofywellness.modals.Meal;
@@ -21,7 +36,12 @@ import java.util.List;
  * Fragment for AddMealTab tab in Home page
  */
 public class AddMealTab extends Fragment {
-    Spinner mealTypeSpinner;
+    /* TODO : Add progress bar while uploading image,
+         and comments required, DB operation to proper folder
+         proper error handling see logcat */
+    private Spinner mealTypeSpinner;
+    private Uri imageuri;
+    private ImageView mealImageView;
     private EditText mealNameTextView, mealEnergyTextView, mealProteinsTextView, mealFatsTextView, mealCarbohydratesTextView;
 
     @Override
@@ -31,7 +51,7 @@ public class AddMealTab extends Fragment {
         View view = inflater.inflate(R.layout.fragment_add_meal_tab, container, false);
 
         // Get the spinner object and set listeners for events
-        mealTypeSpinner = (Spinner) view.findViewById(R.id.meal_type_spinner);
+        mealTypeSpinner = view.findViewById(R.id.meal_type_spinner);
 
         // Add working to the meal type spinner
         addSpinnerForMealType();
@@ -42,29 +62,77 @@ public class AddMealTab extends Fragment {
         mealProteinsTextView = view.findViewById(R.id.meal_protein_field);
         mealFatsTextView = view.findViewById(R.id.meal_fats_field);
         mealCarbohydratesTextView = view.findViewById(R.id.meal_carbohydrates_field);
+        mealImageView = view.findViewById(R.id.meal_add_image_view);
 
-        // Add button to upload today's meal to the database
+        // Add button to upload today's meal to the "DATABASE"
         view.findViewById(R.id.upload_meal_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // Create new meal object to add to database with random url
-                Meal newMeal = new Meal(
-                        "imageulr",
-                        mealNameTextView.getText().toString(),
-                        Integer.parseInt(mealEnergyTextView.getText().toString()),
-                        Integer.parseInt(mealProteinsTextView.getText().toString()),
-                        Integer.parseInt(mealFatsTextView.getText().toString()),
-                        Integer.parseInt(mealCarbohydratesTextView.getText().toString())
-                );
+                if (imageuri != null) {
 
-                // Add meal to the database
-                ofyDatabase.addMeal(newMeal,mealTypeSpinner.getSelectedItem().toString());
+                    uploadImageToFirebaseStorage(imageuri);
+
+                    // Careful now imageuri should point to firebase location instead of a local file
+                    if( imageuri.toString().contains("http")) {
+
+                        // Create new meal object to add to database with random url
+                        Meal newMeal = new Meal(imageuri.toString(), mealNameTextView.getText().toString(), Integer.parseInt(mealEnergyTextView.getText().toString()), Integer.parseInt(mealProteinsTextView.getText().toString()), Integer.parseInt(mealFatsTextView.getText().toString()), Integer.parseInt(mealCarbohydratesTextView.getText().toString()));
+
+                        // Add meal to the database
+                        ofyDatabase.addMeal(newMeal, mealTypeSpinner.getSelectedItem().toString());
+
+                        Toast.makeText(requireActivity(),"Successfully added the meal",Toast.LENGTH_SHORT).show();
+
+                    }
+                    else {
+                        Toast.makeText(requireActivity(),"Successfully added the meal",Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(requireActivity(), "Please select an image ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        view.findViewById(R.id.save_meal_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, 2);
 
             }
         });
 
         return view;
+    }
+    public void uploadImageToFirebaseStorage(Uri uri) {
+
+        StorageReference filereference = FirebaseStorage.getInstance().getReference().child(System.currentTimeMillis() + "." + getFileExtension(imageuri));
+        filereference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                filereference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        imageuri = uri;
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(requireActivity(), "Failed to upload image", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private String getFileExtension(Uri uri) {
+        ContentResolver cr = requireActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
     }
 
     // Method to add spinner for meal type to layout
@@ -83,4 +151,19 @@ public class AddMealTab extends Fragment {
         mealTypeSpinner.setSelection(0);
     }
 
+    // Method to input image from user
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check if successfully got image
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            // Save url to upload to data base
+            imageuri = data.getData();
+            // Set image view to selected image
+            mealImageView.setImageURI(imageuri);
+        }
+        else {
+            Toast.makeText(requireActivity(), "Unable to select image ",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
