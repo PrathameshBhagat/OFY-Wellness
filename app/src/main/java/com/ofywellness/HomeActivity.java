dd34727a44febe67c842f4bf3e917da8a6ea9480
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-07-05 19:26:08 +0530

    Tab show casing functionality added via Tap Target library
    Signed-off-by: Prathamesh Bhagat <prathameshbhagat0608@gmail.com>

--- app/src/main/java/com/ofywellness/HomeActivity.java
+++ app/src/main/java/com/ofywellness/HomeActivity.java
@@ -1,19 +1,14 @@
 package com.ofywellness;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
 import com.google.android.material.tabs.TabLayout;
 import com.ofywellness.fragments.AdapterForTabs;
 
@@ -62,6 +57,50 @@ public class HomeActivity extends AppCompatActivity {
                 tabLayout.getTabAt(position).select();
             }
         });
+
+
+        // Now let the user know what tab has what features and uses and educate him about them
+        educateUserAboutAllTabs();
+
+
+    }
+
+    /**
+     * Creates a sequence of events to showcase all the tab features
+     */
+    void educateUserAboutAllTabs() {
+
+        // Get the tab layout to fetch the tabs
+        TabLayout tablayout = findViewById(R.id.tab_layout);
+
+        // Create a sequence of tap targets to show case all the tabs
+        new TapTargetSequence(this).targets(
+
+                        // Get all the required tap target objects and add to sequence
+                        getTapTarget(tablayout.getTabAt(0).view, R.string.Tab_Hint_1, "You can add all your intakes here"),
+
+                        getTapTarget(tablayout.getTabAt(1).view, R.string.Tab_Hint_2, "You can view all your intakes here"),
+
+                        getTapTarget(tablayout.getTabAt(2).view, R.string.Tab_Hint_3, "You can track all your intakes here"),
+
+                        getTapTarget(tablayout.getTabAt(3).view, R.string.Tab_Hint_4, "You can find all of our services here"),
+
+                        getTapTarget(tablayout.getTabAt(4).view, R.string.Tab_Hint_5, "You can view and edit profile details here")
+
+
+                )
+                // Now start the sequence
+                .start();
+    }
+
+
+    /**
+     * Creates and returns the tab target object for the views and strings
+     */
+    TapTarget getTapTarget(View view, int Title, String description) {
+
+        // Create Tap Target object and return it
+        return TapTarget.forView(view, getString(Title), description).outerCircleColorInt(Color.parseColor("#350069"));
 
     }
 
